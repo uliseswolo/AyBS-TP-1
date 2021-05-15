b020900004259d41836227303b2b03c3e595ab94
uwolo <83558648+uwolo@users.noreply.github.com>
2021-05-15 09:32:06 -0300

    Update main.cpp

--- TP 1/ej 1/main.cpp
+++ TP 1/ej 1/main.cpp
@@ -2,16 +2,23 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <wchar.h>
+#include <locale.h>
 using namespace std;
 
 int main()
-{
+{    
+    setlocale(LC_CTYPE, "Spanish");
+
     float kmh, mts;
+
     cout << "Ingrese el valor en km/h: ";
     cin >> kmh;
+
     mts = kmh / 3.6;
+    
     cout.precision(8);
     cout << "\nResultado en mt/s: " << mts << endl;
 
